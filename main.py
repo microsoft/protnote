Duplicates, gradient clipping, MP, fixed DDP (2/2)

main.py
@@ -1,8 +1,7 @@
-import psutil
 from src.utils.data import (
-    load_model_weights,
+    load_state_dict,
     seed_everything,
-    read_pickle
+    log_gpu_memory_usage
 )
 from src.utils.main_utils import get_or_generate_vocabularies,  get_or_generate_label_embeddings, get_or_generate_sequence_embeddings, validate_arguments
 from src.data.datasets import ProteinDataset, calculate_pos_weight, create_multiple_loaders
@@ -101,9 +100,9 @@ def train_validate_test(gpu, args):
         rank=rank
     )
     print(f"{'=' * 50}\n"
-      f"Initializing GPU {gpu/args.gpus} on node {args.nr};\n"
-      f"    or, gpu {rank+1}/{args.world_size} for all nodes.\n"
-      f"{'=' * 50}")
+          f"Initializing GPU {gpu}/{args.gpus-1} on node {args.nr};\n"
+          f"    or, gpu {rank+1}/{args.world_size} for all nodes.\n"
+          f"{'=' * 50}")
 
     # Check if master process
     is_master = rank == 0
@@ -158,7 +157,8 @@ def train_validate_test(gpu, args):
         subset_fractions={
             "train": params["TRAIN_SUBSET_FRACTION"],
             "validation": params["VALIDATION_SUBSET_FRACTION"],
-            "test": params["TEST_SUBSET_FRACTION"]}
+            "test": params["TEST_SUBSET_FRACTION"]},
+        deduplicate=params["DEDUPLICATE"],
     )
 
     # Seed everything so we don't go crazy
@@ -279,9 +279,8 @@ def train_validate_test(gpu, args):
     if (params["BCE_POS_WEIGHT"] is None) & (args.train_path_name is not None):
         logger.info("Calculating bce_pos_weight...")
         bce_pos_weight = calculate_pos_weight(datasets["train"][0].data,
-                                              datasets["train"][0].get_label_vocabulary_size(
-        )
-        ).to(device)
+                                              len(vocabularies["GO_label_vocab"])
+                                              ).to(device)
         logger.info(f"Calculated bce_pos_weight= {bce_pos_weight.item()}")
     elif (params["BCE_POS_WEIGHT"] is not None):
         bce_pos_weight = torch.tensor(params["BCE_POS_WEIGHT"]).to(device)
@@ -309,7 +308,7 @@ def train_validate_test(gpu, args):
     # Load the model weights if --load-model argument is provided (using the DATA_PATH directory as the root)
     # TODO: Process model loading in the get_setup function
     if args.load_model:
-        load_model_weights(model, os.path.join(
+        load_state_dict(model, os.path.join(
             config["DATA_PATH"], args.load_model))
         logger.info(
             f"Loading model weights from {os.path.join(config['DATA_PATH'], args.load_model)}...")
@@ -340,7 +339,8 @@ def train_validate_test(gpu, args):
     best_val_th = params["DECISION_TH"]
     # Setup for validation
     if args.validation_path_name:
-        val_loader = DataLoader(
+        # Reinitialize the validation loader with all the data, in case we were using a subset to expedite training
+        full_val_loader = DataLoader(
             datasets["validation"][0],
             batch_size=params["TEST_BATCH_SIZE"],
             shuffle=False,
@@ -348,33 +348,44 @@ def train_validate_test(gpu, args):
             num_workers=params["NUM_WORKERS"],
             pin_memory=True,
             sampler=DistributedSampler(
-                datasets["validation"][0], num_replicas=args.world_size, rank=rank)
+                datasets["validation"][0],
+                num_replicas=args.world_size,
+                rank=rank,
+                shuffle=True
+            )
         )
 
         # If no decision threshold is provided, find the optimal threshold on the validation set
         if not params["DECISION_TH"]:
             logger.info("Decision threshold not provided.")
             best_val_th, _ = Trainer.find_optimal_threshold(
-                data_loader=val_loader,
+                data_loader=full_val_loader,
                 optimization_metric_name=params["DECISION_TH_METRIC_NAME"]
             )
 
-        logger.info("====Testing on validation set====")
-        # Final valiadtion using all labels
+        logger.info(
+            f"\n{'='*100}\nTesting on validation set\n{'='*100}")
 
+        # Print the batch size used
+        logger.info(f"Batch size: {params['TEST_BATCH_SIZE']}")
+        if is_master:
+            log_gpu_memory_usage(logger, 0)
+
+        # Final validation using all labels
+        torch.cuda.empty_cache()
         validation_metrics, validation_results = Trainer.evaluate(
-            data_loader=val_loader,
+            data_loader=full_val_loader,
             eval_metrics=eval_metrics.get_metric_collection(type="all",
                                                             threshold=best_val_th,
                                                             num_labels=len(
-                                                                datasets["validation"][0].label_vocabulary)
+                                                                vocabularies["GO_label_vocab"])
                                                             ),
             save_results=True
         )
 
         if is_master:
             save_evaluation_results(results=validation_results,
-                                    label_vocabulary=datasets["validation"][0].label_vocabulary,
+                                    label_vocabulary=vocabularies["GO_label_vocab"],
                                     run_name=args.name,
                                     output_dir=paths["RESULTS_DIR"],
                                     )
@@ -384,16 +395,18 @@ def train_validate_test(gpu, args):
 
     # Setup for testing
     if args.test_paths_names:
-        logger.info("Starting testing...")
         for idx, test_loader in enumerate(loaders["test"]):
-            logger.info(f"====Testing on test set #{idx}====")
-            # If best_val_th is not defined, alert an error to either provide a decision threshold or a validation datapath
+            logger.info(
+                f"\n{'='*100}\nTesting on test set {idx+1}/{len(loaders['test'])}\n{'='*100}")
+            if is_master:
+                log_gpu_memory_usage(logger, 0)
 
+            # TODO: If best_val_th is not defined, alert an error to either provide a decision threshold or a validation datapath
             test_metrics, test_results = Trainer.evaluate(
                 data_loader=test_loader,
                 eval_metrics=eval_metrics.get_metric_collection(type="all",
                                                                 threshold=best_val_th,
-                                                                num_labels=len(datasets["test"][0].label_vocabulary)),
+                                                                num_labels=len(vocabularies["GO_label_vocab"])),
                 save_results=True
             )
 
@@ -402,13 +415,21 @@ def train_validate_test(gpu, args):
             logger.info(json.dumps(test_metrics, indent=4))
             logger.info("Testing complete.")
 
-    # Close the W&B run
+    ####### CLEANUP #######
+    logger.info(
+        f"\n{'='*100}\nTraining, validating, and testing COMPLETE\n{'='*100}\n")
+    # W&B
     if is_master and args.use_wandb and all_test_metrics:
         wandb.log(all_test_metrics[-1])
         wandb.finish()
-
+    # Loggers
+    handlers = logger.handlers[:]
+    for handler in handlers:
+        logger.removeHandler(handler)
+        handler.close()
+    # Torch
+    torch.cuda.empty_cache()
     dist.destroy_process_group()
-    logger.info("################## train.py COMPLETE ##################")
 
 
 """
@@ -421,6 +442,7 @@ def train_validate_test(gpu, args):
     --validation-path-name VAL_DATA_PATH 
     --full-path-name FULL_DATA_PATH 
     --test-paths-names TEST_DATA_PATH
+    --use-wandb
 """
 if __name__ == "__main__":
     main()

src/models/ProTCL.py
@@ -42,39 +42,19 @@ class ProTCL(nn.Module):
             output_neuron_bias=output_neuron_bias
         )
 
-    def _get_joint_embeddings(self, P_e, L_e, num_chunks=os.environ.get("NUM_CHUNKS", 15)):
+    def _get_joint_embeddings(self, P_e, L_e):
         num_sequences = P_e.shape[0]
         num_labels = L_e.shape[0]
         sequence_embedding_dim = P_e.shape[1]
         label_embedding_dim = L_e.shape[1]
 
-        # Calculate chunk size to distribute sequences into equal chunks,
-        # with the last chunk possibly being smaller.
-        chunk_size = (num_sequences + num_chunks - 1) // num_chunks
-
-        joint_embeddings_list = []
-
-        for i in range(0, num_sequences, chunk_size):
-            # Get chunk of protein embeddings. The last chunk may be smaller than chunk_size.
-            P_e_chunk = P_e[i:i + chunk_size]
-            current_chunk_size = P_e_chunk.shape[0]
-
-            # Expand the current chunk of protein and label embeddings
-            # to all combinations of sequences and labels within the chunk.
-            P_e_expanded = P_e_chunk.unsqueeze(
-                1).expand(-1, num_labels, -1).reshape(-1, sequence_embedding_dim)
-
-            # Note: It's important to use current_chunk_size here to ensure that we do not
-            # expand more than necessary in case of the last smaller chunk.
-            L_e_expanded = L_e.unsqueeze(0).expand(
-                current_chunk_size, -1, -1).reshape(-1, label_embedding_dim)
-
-            # Concatenate protein and label embeddings and append to the list.
-            joint_embeddings_list.append(
-                torch.cat([P_e_expanded, L_e_expanded], dim=1))
-
-        # Concatenate all the processed chunks to get the final joint embeddings tensor.
-        joint_embeddings = torch.cat(joint_embeddings_list, dim=0)
+        # Use broadcasting so we don't have to expand the tensor dimensions
+        joint_embeddings = torch.cat([
+            P_e[:, None, :].expand(
+                num_sequences, num_labels, sequence_embedding_dim),
+            L_e[None, :, :].expand(
+                num_sequences, num_labels, label_embedding_dim)
+        ], dim=2).reshape(-1, sequence_embedding_dim + label_embedding_dim)
 
         return joint_embeddings, num_sequences, num_labels
 

src/models/ProTCLTrainer.py
@@ -1,5 +1,5 @@
 import logging
-from src.utils.data import load_gz_json
+from src.utils.data import load_gz_json, log_gpu_memory_usage
 from src.utils.evaluation import EvalMetrics
 from src.utils.losses import BatchWeightedBCE, FocalLoss, RGDBCE
 from torchmetrics import MetricCollection, Metric
@@ -10,6 +10,8 @@ import wandb
 import os
 import json
 from collections import defaultdict
+from torch.cuda.amp import autocast, GradScaler
+from torch.nn.utils import clip_grad_norm_
 
 
 class ProTCLTrainer:
@@ -60,6 +62,7 @@ class ProTCLTrainer:
         self.normalize_probabilities = config["params"]["NORMALIZE_PROBABILITIES"]
         self.validations_per_epoch = config["params"]["VALIDATIONS_PER_EPOCH"]
         self.gradient_accumulation_steps = config["params"]["GRADIENT_ACCUMULATION_STEPS"]
+        self.clip_value = config["params"]["CLIP_VALUE"]
         self.vocabularies = vocabularies
         self.label_normalizer = load_gz_json(
             config["paths"]["PARENTHOOD_LIB_PATH"]
@@ -70,10 +73,11 @@ class ProTCLTrainer:
         self.loss_fn = self._get_loss_fn(config)
         self.model_path = self._get_saved_model_path()
         self.best_val_metric = 0.0
+        self.scaler = GradScaler()
 
     def _get_saved_model_path(self):
         # Save model to OUTPUT_MODEL_DIR. Create path if it doesn't exist.
-        if not os.path.exists(self.output_model_dir):
+        if not os.path.exists(self.output_model_dir) and self.is_master:
             os.makedirs(self.output_model_dir)
 
         model_name = (
@@ -108,7 +112,7 @@ class ProTCLTrainer:
         trainable_params = []
         trainable_params_names = []
 
-        for name, param in self.model.named_parameters():
+        for name, param in self.model.module.named_parameters():
             if name.startswith('sequence_encoder') and (not self.train_sequence_encoder):
                 param.requires_grad = False
 
@@ -160,7 +164,8 @@ class ProTCLTrainer:
             "tokenized_labels": tokenized_labels,
             "label_embeddings": label_embeddings
         }
-        logits = self.model(**inputs)
+        with autocast():
+            logits = self.model(**inputs)
 
         # Compute validation loss for the batch
         loss = self.loss_fn(logits, label_multihots.float())
@@ -190,7 +195,7 @@ class ProTCLTrainer:
             )
             self.best_val_metric = val_metrics[val_optimization_metric_name]
             # TODO: Break out model saving into separate method (save_checkpoint)
-            torch.save(self.model.state_dict(), self.model_path)
+            torch.save(self.model.module.state_dict(), self.model_path)
             self.logger.info(f"Saved model to {self.model_path}.")
 
             if self.use_wandb:
@@ -258,7 +263,7 @@ class ProTCLTrainer:
             if score > best_score:
                 best_score = score
                 best_th = th
-            print("TH:", th, "F1:", score)
+            self.logger.info("TH: {:.3f}, F1: {:.3f}".format(th, score))
 
         best_score = best_score
         self.logger.info(
@@ -358,8 +363,6 @@ class ProTCLTrainer:
         :param val_loader: _description_
         :type val_loader: torch.utils.data.DataLoader
         """
-        # Log that training is starting
-        self.logger.info("Starting training...")
         self.model.train()
 
         # Watch the model
@@ -368,16 +371,27 @@ class ProTCLTrainer:
 
         # Compute total number of training steps
         num_training_steps = len(train_loader) * self.num_epochs
+        self.logger.info(f"{'='*100}")
         self.logger.info(
-            f"Total number of training steps: {num_training_steps}")
+            f"Starting training. Total number of training steps: {num_training_steps}")
+        self.logger.info(f"{'='*100}")
         batch_count = 0
 
         for epoch in range(self.num_epochs):
             self.logger.info(
                 f"Starting epoch {epoch+1}/{self.num_epochs}...")
+
+            if self.is_master:
+                log_gpu_memory_usage(self.logger, 0)
+
+            # Set distributed loader epoch to shuffle data
+            if hasattr(train_loader.sampler, "set_epoch"):
+                train_loader.sampler.set_epoch(epoch)
+
             ####### TRAINING LOOP #######
             for batch in train_loader:
-                # Increment batch index
+                # Increment batch index and set gradients to zero
+                self.optimizer.zero_grad()
                 batch_count += 1
 
                 # Unpack the training batch
@@ -402,7 +416,9 @@ class ProTCLTrainer:
                     "tokenized_labels": tokenized_labels,
                     "label_embeddings": label_embeddings
                 }
-                logits = self.model(**inputs)
+
+                with autocast():
+                    logits = self.model(**inputs)
 
                 # log average probabilities to W&B
                 if self.use_wandb:
@@ -423,13 +439,16 @@ class ProTCLTrainer:
                 if self.use_wandb:
                     wandb.log({"train_loss": loss.item()})
 
-                # Backward pass
-                loss.backward()
+                # Backward pass with mixed precision
+                self.scaler.scale(loss).backward()
 
                 # Gradient accumulation every GRADIENT_ACCUMULATION_STEPS
                 if (batch_count % self.gradient_accumulation_steps == 0):
-                    self.optimizer.step()
-                    self.optimizer.zero_grad()
+                    # Apply gradient clipping
+                    clip_grad_norm_(self.model.module.parameters(),
+                                    max_norm=self.clip_value)
+                    self.scaler.step(self.optimizer)
+                    self.scaler.update()
 
                 # Log training progress percentage every 2%
                 if num_training_steps > 100 and batch_count % int(num_training_steps/100) == 0:
@@ -456,6 +475,15 @@ class ProTCLTrainer:
                     self.logger.info(
                         f"Validation metrics:\n{json.dumps(val_metrics, indent=4)}")
 
-        # Set weights to best model
-        self.logger.info("Restoring model to best validation map_micro...")
-        self.model.load_state_dict(torch.load(self.model_path))
+        if self.is_master:
+            self.logger.info("Restoring model to best validation map_micro...")
+            self.model.module.load_state_dict(torch.load(self.model_path))
+
+            # Broadcast model state to other processes
+            for param in self.model.module.parameters():
+                # src=0 means source is the master process
+                torch.distributed.broadcast(param.data, src=0)
+        else:
+            # For non-master processes, just receive the broadcasted data
+            for param in self.model.module.parameters():
+                torch.distributed.broadcast(param.data, src=0)

src/utils/data.py
@@ -9,6 +9,39 @@ import random
 import numpy as np
 import wget
 import hashlib
+from collections import OrderedDict
+
+
+def log_gpu_memory_usage(logger, device_id):
+    device = torch.device(f"cuda:{device_id}")
+    allocated = torch.cuda.memory_allocated(
+        device) / (1024 ** 2)  # Convert bytes to MB
+    reserved = torch.cuda.memory_reserved(
+        device) / (1024 ** 2)  # Convert bytes to MB
+    total_memory = torch.cuda.get_device_properties(
+        device).total_memory / (1024 ** 2)  # Convert bytes to MB
+
+    allocated_percent = (allocated / total_memory) * 100
+    reserved_percent = (reserved / total_memory) * 100
+    combined = allocated + reserved
+    combined_percent = (combined / total_memory) * 100
+    logger.info("-" * 50)
+    logger.info(
+        f"Device {device_id} [Name: {torch.cuda.get_device_name(device)}]")
+    logger.info(
+        f"  Allocated Memory: {allocated:.2f} MB ({allocated_percent:.2f}%)")
+    logger.info(
+        f"  Reserved Memory: {reserved:.2f} MB ({reserved_percent:.2f}%)")
+    logger.info(
+        f"  Combined (Allocated + Reserved) Memory: {combined:.2f} MB ({combined_percent:.2f}%)")
+    logger.info(f"  Total Device Memory: {total_memory:.2f} MB")
+    logger.info("-" * 50)
+
+
+def convert_float16_to_float32(df):
+    float16_cols = df.select_dtypes(include='float16').columns
+    df[float16_cols] = df[float16_cols].astype('float32')
+    return df
 
 
 def hash_alphanumeric_sequence_id(s: str):
@@ -83,12 +116,33 @@ def download_and_unzip(url, output_file):
         f"File {output_file + '.gz'} has been downloaded and unzipped to {output_file}.")
 
 
-def load_model_weights(model, path):
+def load_state_dict(model, checkpoint_path):
     """
-    Loads PyTorch model weights from a .pt file.
+    Load the model's state dict from the checkpoint.
+
+    This function handles both DDP-wrapped and non-DDP checkpoints.
+
+    :param model: The model into which the checkpoint's state dict should be loaded.
+    :param checkpoint_path: Path to the checkpoint file.
+    :return: The model with loaded state dict.
     """
-    assert path and os.path.exists(path), f"Model weights not found at {path}."
-    model.load_state_dict(torch.load(path))
+
+    # Load checkpoint's state dict
+    state_dict = torch.load(checkpoint_path)
+
+    # Check if the state_dict is NOT from a DDP-wrapped model
+    if not list(state_dict.keys())[0].startswith('module.'):
+        # Create a new OrderedDict with the "module." prefix
+        new_state_dict = OrderedDict()
+        for k, v in state_dict.items():
+            name = 'module.' + k  # add 'module.' prefix
+            new_state_dict[name] = v
+        state_dict = new_state_dict
+
+    # Load the state_dict into the model
+    model.load_state_dict(state_dict)
+
+    return model
 
 
 def seed_everything(seed: int, device: str):